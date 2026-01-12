b03aaf43c121b6f7d5f4c37f47cfae0524d718bd
RafaSSii <rafarsk78@gmail.com>
2026-01-12 14:49:06 -0300

    feat: project structure

---
+++ src/main/java/br/com/rafoso/gestao_custos/GestaoDeCustosPessaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafoso.gestao_custos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeCustosPessaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeCustosPessaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafoso/gestao_custos/GestaoDeCustosPessaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafoso.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCustosPessaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
