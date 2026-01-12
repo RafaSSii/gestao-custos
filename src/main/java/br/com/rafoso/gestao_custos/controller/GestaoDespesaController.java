4bedd7e119ea3144ffd2a2604d50e7358faa4fb8
RafaSSii <rafarsk78@gmail.com>
2026-01-12 17:12:16 -0300

    feat: database creation

--- src/main/java/br/com/rafoso/gestao_custos/controller/GestaoDespesaController.java
+++ src/main/java/br/com/rafoso/gestao_custos/controller/GestaoDespesaController.java
@@ -1,13 +1,23 @@
 package br.com.rafoso.gestao_custos.controller;
 
+import br.com.rafoso.gestao_custos.model.Despesa;
+import br.com.rafoso.gestao_custos.useCases.CadastroDespesaUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/gestao")
+@RestController
 public class GestaoDespesaController {
 
-    @PostMapping("/create")
-    public void create(){
+    @Autowired
+    CadastroDespesaUseCase cadastroDespesaUseCase;
 
+    @PostMapping("/create")
+    public void create(@RequestBody Despesa despesa){
+
+        cadastroDespesaUseCase.execute(despesa);
     }
 }
---
+++ src/main/java/br/com/rafoso/gestao_custos/useCases/CadastroDespesaUseCase.java
@@ -0,0 +1,12 @@
+package br.com.rafoso.gestao_custos.useCases;
+
+import br.com.rafoso.gestao_custos.model.Despesa;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroDespesaUseCase {
+    public void execute(Despesa despesa){
+
+    }
+
+}
